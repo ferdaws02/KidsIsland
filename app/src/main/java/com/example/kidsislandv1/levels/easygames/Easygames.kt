fa7baa9018332698d379b3c10462132e0e167396
ferdaws02 <ferdawslaamouri2@gmail.com>
2022-01-02 00:15:54 +0100

    ajout des diamant, navigation et retour avec une appbar

--- app/src/main/java/com/example/kidsislandv1/levels/easygames/Easygames.kt
+++ app/src/main/java/com/example/kidsislandv1/levels/easygames/Easygames.kt
@@ -7,11 +7,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.RecyclerView
 import com.example.kidsislandv1.R
 import com.example.kidsislandv1.levels.easygames.memorygame.MemoryLevel
 import com.example.kidsislandv1.levels.easygames.memorygame.MemoryMainActivity
 import com.example.kidsislandv1.levels.easygames.puzzelgame.MainpuzzelActivity
+import kotlinx.android.synthetic.main.activity_easygames.*
 
 class Easygames : AppCompatActivity() {
 
@@ -31,11 +33,30 @@ class Easygames : AppCompatActivity() {
         setContentView(R.layout.activity_easygames)
 
 
+        val back:ImageView = findViewById(R.id.back)
+
         val intro :ImageView =findViewById(R.id.puzzelin)
         val memory :ImageView =findViewById(R.id.memory)
 
         loadData()
+back.setOnClickListener { onBackPressed () }
 
+        val sharedPreferences = getSharedPreferences( "sharedPrefs", Context.MODE_PRIVATE)
+        val savedString = sharedPreferences.getString( "STRING_KEY",  null)
+
+
+        var selectavatarbutton : ImageView = findViewById(R.id.idbtnselectavatar)
+
+        var drawableRessource = when (savedString){
+
+            "avatar1"->R.drawable.avatar1
+            "avatar2"->R.drawable.avatar2
+            "avatar3"->R.drawable.avatar3
+            "avatar4"->R.drawable.avatar4
+            "avatar5"->R.drawable.avatar5
+            else ->R.drawable.avatar6
+        }
+        selectavatarbutton.setImageResource(drawableRessource)
         intro.setOnClickListener {
 
             var soudClick = R.raw.sound_button
